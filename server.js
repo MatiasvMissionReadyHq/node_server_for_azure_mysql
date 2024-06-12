25f4b7c623ee223d7f57df36765eaf4dc8c15822
MatiasvMissionReadyHq <150482921+MatiasvMissionReadyHq@users.noreply.github.com>
2024-06-12 22:05:02 +1200

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ const corsOptions = {
   origin: "http://localhost:5174"
 };
 
+function setCorsHeaders(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+}
+
 app.use(cors(corsOptions));
+app.use(setCorsHeaders);
+
 
 //app.use(cors({ origin: process.env.CLIENT_HOST }));
 
